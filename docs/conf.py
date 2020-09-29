1348b76b502afe114d18b1841b7e92c6ed23b4a0
Sergio Portoles <sergio.portolesdiez@kuleuven.be>
2020-09-29 15:42:22 +0200

    move static files to '_static' folder
    The PDF generated from the repository
    <https://github.com/orocos/orocos-bayesian-filtering>
    should be considered static files and therefore
    placed in a static folder for sphinx generation.

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', '_static', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -105,7 +105,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
